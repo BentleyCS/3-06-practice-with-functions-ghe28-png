7ff3e8910eedb6ac79ecf3d556495e14689738fd
ghe28-png <ghe28@bentleyschool.org>
2025-11-06 10:59:53 -0800

    Add files via upload

--- CSP_3_06_Practice_with_functions.py
+++ CSP_3_06_Practice_with_functions.py
@@ -10,12 +10,13 @@ def root(n):
 def semiPerimeter(num1,num2,num3):
     return(num1/2+num2/2+num3/2)
 
+numbers1=int(input("Give me one side of the trangle"))
+numbers2=int(input("Give me one side of the trangle"))
+numbers3=int(input("Give me one side of the trangle"))
+answer2=semiPerimeter(numbers1,numbers2,numbers3)
+print(answer2)
     pass
-# number1=int(input("Give me an number"))
-# number2=int(input("Give me an number"))
-# number3=int(input("Give me an number"))
-# ans=semiPerimeter(number1,number2,number3)
-# print(ans)
+
 
 def multiplyDifferences():
     pass
@@ -23,18 +24,9 @@ def multiplyDifferences():
 #Given the 3 sides of a triangle return the area.
 #use herons formula
 #Use the functions above.
-def herons(num1,num2,num3):
-    a=semiPerimeter(num1,num2,num3)-num1
-    b=semiPerimeter(num1,num2,num3)-num2
-    c=semiPerimeter(num1,num2,num3)-num3
-    d=semiPerimeter(num1,num2,num3)
-    return math.sqrt(a*b*c*d)
+def herons():
     pass
-# number4=int(input("Give me an number"))
-# number5=int(input("Give me an number"))
-# number6=int(input("Give me an number"))
-# ans1=herons(number4,number5,number6)
-# print(ans1)
+
 
 #quadratic equation
 
@@ -42,31 +34,21 @@ def herons(num1,num2,num3):
 def denominator(num1):
     return num1*2
     pass
-# number7=int(input("Give me an number"))
-# ans2=denominator(number7)
-# print(ans2)
+number=input("Give me an number")
+answer1=denominator(number)
+print(answer1)
+
 #Takes in two arguments. multiply the first argument by negative 1. Then
 #return the modified first argument added and subtracted by the second argument.
-def plusMinus(num1,num2):
-    product=num1*num2
-    product1=product+num2
-    product2 = product - num2
-    return  product1 and product2
+def plusMinus():
     pass
-
 #takes in three numbers as arguments. The first and third multiplied together then
 #multiplied by 4.Then subtract that result from the second argument squared.
 #Return the overall result.
-def mainCalc(num1,num2,num3):
-    product1=num1*num3*4
-    squared=num2*num2
-    return squared*product1
+def mainCalc():
     pass
 
 #The below function should take the inputs of the quadratic equation and return the result
 #Make sure to use all the formulas from this section.
-def quadratic(a,b,c):
-    delta=b*b-4*a*c
-    result=-b/2/a+math.sqrt(delta)/a/2
-    return result
+def quadratic():
     pass
